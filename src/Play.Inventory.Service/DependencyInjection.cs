c43b8c8c37b285fce8e3e81984219dd0b2fe420c
Adriano Mota <adrianowsh@gmail.com>
2024-06-24 19:27:52 -0300

    add polly in order to retry http requests

--- src/Play.Inventory.Service/DependencyInjection.cs
+++ src/Play.Inventory.Service/DependencyInjection.cs
@@ -3,11 +3,14 @@ using Play.Common;
 using Play.Common.MongoDb;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 
 namespace Play.Inventory.Service;
 
 public static class DependencyInjection
 {
+
     public static IServiceCollection AddRepositories(this IServiceCollection services)
     {
         services.AddSingleton<IRepository<InventoryItem>>(static serviceProvider =>
@@ -17,14 +20,26 @@ public static class DependencyInjection
         });
         return services;
     }
-
-
     public static IServiceCollection AddHttpCatalogClient(this IServiceCollection services)
     {
+        Random jitterer = new();
+
         services.AddHttpClient<CatalogClient>(client =>
         {
             client.BaseAddress = new Uri("https://localhost:5001");
-        });
+        })
+        .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+            retryCount: 5,
+            retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                             + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+            onRetry: (outcome, timespan, retryAttempt) =>
+            {
+                var serviceProvider = services.BuildServiceProvider();
+                serviceProvider.GetService<ILogger<CatalogClient>>()?
+                    .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+            }
+        ))
+        .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(1)));
         return services;
     }
 }
