a4b3bd652f8a2bbb4dde4a34209819a845748885
Adriano Mota <adrianowsh@gmail.com>
2024-06-28 16:44:20 -0300

    add jwt bearer token from play.common

--- src/Play.Inventory.Service/Controllers/ItemsController.cs
+++ src/Play.Inventory.Service/Controllers/ItemsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Play.Common;
 using Play.Inventory.Service.Clients;
@@ -6,6 +7,7 @@ using Play.Inventory.Service.Entities;
 
 namespace Play.Inventory.Service.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/items")]
 public class ItemsController : ControllerBase
--- src/Play.Inventory.Service/Program.cs
+++ src/Play.Inventory.Service/Program.cs
@@ -1,8 +1,10 @@
+using Play.Common.Identity;
 using Play.Common.MassTransitRabbitMq;
 using Play.Common.MongoDb;
 using Play.Inventory.Service;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddJwtBearerAuthentication();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 app.ConfigureCors(builder.Configuration);
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
